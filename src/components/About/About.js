db74da22d04571a65069ddd1724f1dfd7a6656d3
frankieag10 <frankieag10@gmail.com>
2023-10-24 14:58:34 -0700

    about/app

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+  return (
+    <section className="about">
+      <h1 className="about__header">About</h1>
+      <p className="about__info">
+        Hello Im frank! Iam a crane techincian on my way to becoming a software engineer. I love sports such as football and baseball and basketball
+        and I also love great music!
+      </p>
+    </section>
+  );
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Footer from "../Footer/Footer";
+import Main from "../Main/Main";
 
 function App() {
   return <div className="App"></div>;
