ce92b540eed8f8265048aafc3f072b810ad49ef4
frankieag10 <frankieag10@gmail.com>
2023-10-24 22:30:47 -0700

    app mod

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import "../../vendor/fonts.css";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
 import Header from "../Header/Header";
@@ -8,7 +9,14 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [username, setUserName] = useState("");
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header
+        loggedIn={loggedIn}
+        username={username}
+      />
+    </div>
+  );
 }
 
 export default App;
