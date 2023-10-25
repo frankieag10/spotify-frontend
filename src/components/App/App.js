ad42bcd20e43c91747cdec2dfe91c4aad1357e78
frankieag10 <frankieag10@gmail.com>
2023-10-24 22:22:58 -0700

    updates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
+import Header from "../Header/Header";
 
 function App() {
   return <div className="App"></div>;
