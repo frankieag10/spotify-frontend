58fe94f91f5b195750b85a7ec34b07ba502aed6b
frankieag10 <frankieag10@gmail.com>
2023-10-30 08:12:56 -0700

    songModal mods

---
+++ src/components/SongModal/SongModal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SongModal.css";
+
+export default function SongModal({ song, onClose }) {
+  return (
+    <div className="modal">
+      <div className="modal__container"></div>
+    </div>
+  );
+}
