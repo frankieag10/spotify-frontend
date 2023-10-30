e535960ed73831ebe1c9e4026ab43cff874b3630
frankieag10 <frankieag10@gmail.com>
2023-10-30 15:02:26 -0700

    app updates

---
+++ src/components/utils/spotify.js
@@ -0,0 +1,6 @@
+const CLIENT_ID = "ae0e690257e84280835e3efc4e0b7862";
+const REDIRECT_URL = window.location.origin;
+const SPOTIFY_URL = "https://api.spotify.com/v1/";
+const SCOPE = "user-read-private user-read-email user-top-read";
+const SPOTIFY_LOCAL_STORAGE_KEY = "spotify_token";
+const SPOTIFY_VERIFIER_STORAGE_KEY = "spotify_verifier";
