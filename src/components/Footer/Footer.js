a28f50bc26524cf9fe58e00d402d0e84efff9073
frankieag10 <frankieag10@gmail.com>
2023-10-30 12:23:58 -0700

    updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import About from "../About/About";
 export default function Footer() {
   return (
     <footer className="footer__container">
+      <About />
       <div className="footer">
         <address className="footer__author">Developed by: Frank Aguirre</address>
         <time className="footer__year">2023</time>
