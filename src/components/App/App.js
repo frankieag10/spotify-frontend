82501b5c531b538773db26149af0547e5efb4fae
frankieag10 <frankieag10@gmail.com>
2023-10-30 10:02:15 -0700

    songModal fix css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ import SongModal from "../SongModal/SongModal";
 import Header from "../Header/Header";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
   const [username, setUserName] = useState("");
   const [activeModal, setActiveModal] = useState(null);
 
