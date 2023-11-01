fad893bd7a9de28a606276b5cc9e1cc1d52589fc
frankieag10 <frankieag10@gmail.com>
2023-10-31 17:40:55 -0700

    about.js fix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,8 +6,8 @@ export default function About() {
     <section className="about">
       <h1 className="about__header">About</h1>
       <p className="about__info">
-        Hello Im Frank! Iam a crane techincian on my way to becoming a software engineer. I love sports such as football and baseball and basketball
-        and I also love great music!
+        Hello Im Frank! I'm currently my way to becoming a software engineer. I love sports such as football and baseball and basketball and I also
+        love great music!
       </p>
     </section>
   );
